6286e8d45422c77022183fe73f064dd2ccf9309a
Veronika Dodda <v.shengeliya@gmail.com>
2020-01-04 05:05:25 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,10 +1,21 @@
+# def square_array(array)
+#   new_array = []
+#   i = 0 
+#   while i < array.size do
+#   new_array << (array(i)**2)
+#   i += 1
+#   end 
+#   new_array
+# end
+
+
 def square_array(array)
-  new_array = []
-  i = 0 
-  while i < array.size do
-  new_array << (array(i)**2)
-  i += 1
-  end 
-  new_array
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
 
